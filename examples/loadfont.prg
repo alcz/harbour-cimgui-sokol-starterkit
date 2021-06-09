0704ce2d77ef314b267a02c4cd74508025fc9e70
Aleksander Czajczynski <hb@fki.pl>
2021-06-09 12:13:05 +0200

    2021-06-09 12:11 UTC+0200 Aleksander Czajczynski (hb fki.pl)   + sokol/sokol.hbp   + sokol/hbsokol.c     + added default minimally configurable application template, for use       by examples.       Parameters specification are:       sapp_default_run( <cWindowCaption>, [ <nWidth> ], [ <nHeight> ] )
        + hb_sokol_imguiNoDefaultFont( <lSet> ) which should be called with .T.
          parameter before sapp_*_run(), to prevent generating textures for
          Dear ImGui default font.
    
      +  examples/OpenSans-Regular.ttf
      +  examples/loadfont.prg
      +  examples/hbmk.hbm
      +  examples/wmake.sh
         + added example of loading custom font, which is important functionality,
           as OS font rendering is not used by Dear ImGui.
           Build by calling "hbmk2 loadfont.prg". For emscripten/WebAssembly
           try with helper script "wmake.sh loadfont.prg"

---
+++ examples/loadfont.prg
@@ -0,0 +1,70 @@
+/*
+    loadfont.prg    -- example of font loading facilities
+
+    license is MIT, see ../LICENSE
+*/
+
+#include "hbimenum.ch"
+
+REQUEST HB_CODEPAGE_PL852
+REQUEST HB_CODEPAGE_EL737
+REQUEST HB_CODEPAGE_UTF8EX
+
+PROCEDURE MAIN
+#ifndef __PLATFORM__WEB
+   IF ! File( "OpenSans-Regular.ttf" )
+      Alert("can't find my font")
+   ENDIF
+#endif
+   hb_sokol_imguiNoDefaultFont( .T. )
+   sapp_run_default( "Custom Font Example", 800, 600 )
+
+#ifdef __PLATFORM__WEB
+   IF ImFrame() # NIL /* dummy calls for emscripten, to be removed when those functions are properly requested from .c code */
+      ImInit()
+   ENDIF
+#endif
+   RETURN
+
+PROCEDURE ImInit
+#ifdef __PLATFORM__WEB
+   LOCAL cFontBuf
+#pragma __binarystreaminclude "OpenSans-Regular.ttf"|cFontBuf := %s
+   hb_igAddFontFromMemoryTTF( cFontBuf, 18.0, , { "EL737", "PL852" }, .T., .F. )
+#else
+   hb_igAddFontFromFileTTF( "OpenSans-Regular.ttf", 18.0, , { "EL737", "PL852" }, .T., .F. )
+#endif
+
+   hb_sokol_imguiFont2Texture()
+   RETURN
+
+PROCEDURE ImFrame
+   STATIC counter := 0, s, c
+
+   igSetNextWindowPos( {10,10}, ImGuiCond_Once, {0,0} )
+   igSetNextWindowSize( {650, 350}, ImGuiCond_Once )
+   igBegin( "Hello Dear ImGui!", 0, ImGuiWindowFlags_None )
+
+   igText("Viel Glück! α Ω")
+
+   IF IgButton( "dupa.8 dópą.8" )
+      counter++
+   ENDIF
+
+   igSameLine(0.0, -1.0)
+   igText("counter " + hb_NtoS( counter ) )
+
+   igSliderFloat("float", @s, 0.0, 1.0, "%.2f", 0)
+   igText("slider updates Harbour variable -> " + hb_NtoS( s ) )
+
+   IF c == NIL
+      c := ""
+   ENDIF
+
+   igInputTextWithHint("input text (w/ hint)", "enter text here", @c, 200 );
+
+   igText("UPPER() by Harbour -> " + Upper( c ) )
+
+   igEnd()
+
+   RETURN
---
+++ sokol/hbsokol.c
@@ -0,0 +1,109 @@
+/*
+    sokol/hbsokol.c    -- default application structure, used by examples/
+
+    license is MIT, see ../LICENSE
+
+    Copyright (c) 2021 Aleksander Czajczynski
+*/
+
+
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_glue.h"
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
+#include "sokol_imgui.h"
+#include "hbapi.h"
+#include "hbvm.h"
+
+static HB_BOOL s_bNoDefaultFont = HB_FALSE;
+
+static struct {
+    uint64_t laptime;
+    sg_pass_action pass_action;
+} state;
+
+static void init(void) {
+    PHB_DYNS pDynSym = NULL;
+
+    sg_setup(&(sg_desc){
+        .context = sapp_sgcontext()
+    });
+    stm_setup();
+    simgui_setup(&(simgui_desc_t){ .no_default_font = s_bNoDefaultFont });
+
+    if( ( pDynSym = hb_dynsymFindName( "IMINIT" ) ) )
+    {
+       hb_vmPushDynSym( pDynSym );
+       hb_vmPushNil();
+       hb_vmProc( 0 );
+    }
+
+    // initial clear color
+    state.pass_action = (sg_pass_action) {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .value = { 0.0f, 0.5f, 1.0f, 1.0 } }
+    };
+}
+
+static void frame(void) {
+    static PHB_DYNS pDynSym = NULL;
+    const int width = sapp_width();
+    const int height = sapp_height();
+    const double delta_time = stm_sec(stm_round_to_common_refresh_rate(stm_laptime(&state.laptime)));
+    simgui_new_frame(width, height, delta_time);
+
+    if( ! pDynSym )
+       pDynSym = hb_dynsymFindName( "IMFRAME" );
+
+    /*=== UI CODE STARTS HERE === */
+
+    if( pDynSym )
+    {
+       hb_vmPushDynSym( pDynSym );
+       hb_vmPushNil();
+       hb_vmProc( 0 );
+    }
+
+    /*=== UI CODE ENDS HERE ===*/
+
+    sg_begin_default_pass(&state.pass_action, width, height);
+    simgui_render();
+    sg_end_pass();
+    sg_commit();
+}
+
+static void cleanup(void) {
+    simgui_shutdown();
+    sg_shutdown();
+}
+
+static void event(const sapp_event* ev) {
+    simgui_handle_event(ev);
+}
+
+static sapp_desc hb_sokol_main( const char * pszCaption, int width, int height ) {
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .event_cb = event,
+        .window_title = pszCaption,
+        .width = width,
+        .height = height,
+    };
+}
+
+HB_FUNC( HB_SOKOL_IMGUINODEFAULTFONT )
+{ 
+   HB_BOOL bRet = s_bNoDefaultFont;
+
+   s_bNoDefaultFont = hb_parl( 1 );
+   hb_retl( bRet );
+}
+
+HB_FUNC( SAPP_RUN_DEFAULT )
+{ 
+   sapp_desc s = hb_sokol_main( hb_parcx( 1 ), hb_parnidef( 2, 800 ), hb_parnidef( 3, 600 ) );
+   sapp_run( &s );
+}
